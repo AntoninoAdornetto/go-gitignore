dd5dc7da5d9343b00a549962a48caf0927ffcb5d
AntoninoAdornetto <armaninollc@protonmail.com>
2024-06-02 20:29:26 -0400

    chore: .gitignore & .git/info/exclude exc groups

--- ignore.go
+++ ignore.go
@@ -1,5 +1,10 @@
 package ignore
 
+import (
+	"os"
+	"path/filepath"
+)
+
 type Bits = uint8
 
 const (
@@ -27,3 +32,43 @@ type IgnorePattern struct {
 	Pattern         string
 	OriginalPattern string
 }
+
+func NewIgnorer(absPath string) (*Ignorer, error) {
+	ig := &Ignorer{}
+
+	ignorePath := filepath.Join(absPath, ".gitignore")
+	excludePath := filepath.Join(absPath, ".git", "info", "exclude")
+
+	if err := ig.AppendExcludeGroup(ignorePath, ".gitignore"); err != nil {
+		return nil, err
+	}
+
+	if err := ig.AppendExcludeGroup(excludePath, "exclude"); err != nil {
+		return nil, err
+	}
+
+	return ig, nil
+}
+
+func NewExcludeGroup(path, src string) (*ExcludeGroup, error) {
+	group := &ExcludeGroup{Src: src, BasePath: filepath.Clean(path)}
+
+	iFile, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	defer iFile.Close()
+	return group, nil
+}
+
+func (ig *Ignorer) AppendExcludeGroup(path, src string) error {
+	group, err := NewExcludeGroup(path, src)
+	if err != nil {
+		return err
+	}
+
+	ig.ExcludeGroups = append(ig.ExcludeGroups, *group)
+	return nil
+}
+
